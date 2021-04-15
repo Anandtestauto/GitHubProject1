df20c4fd34b7ab4264fc3ea46a65217ba5bed475
Anandtestauto <66576330+Anandtestauto@users.noreply.github.com>
2021-04-15 11:26:35 +0530

    commiting to branch1

---
+++ src/main/java/Samplepacksge/Sampleclass.java
@@ -0,0 +1,20 @@
+package Samplepacksge;
+
+public class Sampleclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		   
+		   int i=5;
+		   
+		   if(i%2==0) {
+			   System.out.println("its an even number");
+		   }
+		 
+		   else {
+			   System.out.println("its an odd number");
+		   }
+	}
+
+}
